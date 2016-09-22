477ded018b57f68927295715d11ac0907de1d73f
Michael Grosse <grosse@cosmocode.de>
2016-09-22 12:17:56 +0200

    Add layout information to secedit form
    That way we can use it in edittable

--- script.js
+++ script.js
@@ -19,6 +19,15 @@ jQuery(function(){
     jQuery('.plugin_tablelayout_placeholder').each(function (index, element) {
         var id = jQuery(element).data('tablelayout');
         var $table = jQuery(element).next().find('table');
+
+        if (typeof JSINFO.plugin == 'undefined') {
+            console.dir(JSINFO);
+            return;
+        }
+
+        var $secedit_form = jQuery(element).siblings('.secedit').find('form div.no');
+        var $input = jQuery('<input name="tablelayout" type="hidden">').val(JSON.stringify(JSINFO.plugin.tablelayout[id]));
+        $secedit_form.prepend($input);
         if (JSINFO.plugin.tablelayout[id].colwidth.length) {
             var numCols = tablelayout.getNumberOfTableCols($table);
             var $colgroup = jQuery('<colgroup>');
