42c12b19c318c2105fb1dd32830867a3ac7168dd
Michael Grosse <grosse@cosmocode.de>
2016-10-07 15:03:40 +0200

    Add German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * German language file for tablelayout plugin
+ *
+ * @author Michael Große <dokuwiki@cosmocode.de>
+ */
+
+$lang['legend:tablelayout'] = 'Tabellenlayout anpassen';
+$lang['button:apply'] = 'Anwenden & Vorschau';
+$lang['label:rowsFixed'] = 'Anzahl der fixierten Kopfzeilen';
+$lang['label:rowsVisible'] = 'Anzahl der weiteren sichtbaren Zeilen';
+$lang['label:alignment'] = 'Ausrichtung der Tabelle:';
+$lang['option:default'] = 'Standard';
+$lang['option:float left'] = 'Linksbündig umfließend';
+$lang['option:float right'] = 'Rechtsbündig umfließend';
+$lang['option:center'] = 'Zentriert';
+$lang['js']['loading'] = 'Lade...';
+
+//Setup VIM: ex: et ts=4 :
